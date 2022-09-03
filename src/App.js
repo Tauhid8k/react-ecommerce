9de9379eb8a42e828571be7053d9cf7ebbb8f153
Tauhid8k <tauhid8k@gmail.com>
2022-09-04 04:57:19 +0600

    home screen & redux store setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Container } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <main className='py-4'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
+        </Container>
+      </main>
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
